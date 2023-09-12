cde08f0663ec49a268ace95da053796e0925671d
Nomadyb <Nomadaeyb@outlook.com>
2023-09-12 22:37:50 +0300

    update from github

--- designWork1/designWork1/ViewController.swift
+++ designWork1/designWork1/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		self.navigationItem.title = "Pizza"
+		
+		let apperance = UINavigationBarAppearance()
+		apperance.backgroundColor = UIColor(named: "mainColor")
+		apperance.titleTextAttributes = [.foregroundColor: UIColor(named: "textColor1")!,.font:UIFont(name: "NotoSans-BlackItalic", size: 25)!]
+		
+		navigationController?.navigationBar.compactScrollEdgeAppearance = apperance
+		
+		
+		navigationController?.navigationBar.standardAppearance = apperance
+		
+		navigationController?.navigationBar.compactAppearance = apperance
+		
+		navigationController?.navigationBar.scrollEdgeAppearance = apperance
+		
 	}
 
 
