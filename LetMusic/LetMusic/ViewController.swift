d6318eec6fe2e36191926c5f58b9993aa03488d6
Nomadyb <Nomadaeyb@outlook.com>
2023-09-16 22:11:00 +0300

    personal access for the project

--- LetMusic/LetMusic/ViewController.swift
+++ LetMusic/LetMusic/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
+	
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+
+
+		let navbarColor = UIColor(red: 10/255.0, green: 9/255.0, blue: 30/255.0, alpha: 1.0)
+		let darkPurple = UIColor(red: 120/255.0, green: 0, blue: 120/255.0, alpha: 1.0)
+		
+		let gradient = CAGradientLayer()
+		gradient.colors = [darkPurple.cgColor, navbarColor.cgColor]
+		gradient.locations = [0.0, 1.0]
+		gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+		gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+		gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+
+		self.view.layer.insertSublayer(gradient, at: 0)
+	}
+
+
+
+
 
 
 }
