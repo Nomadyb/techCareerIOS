a2222260f0339a7a42b9d9bcf359ac4086c46f65
Nomadyb <51696895+Nomadyb@users.noreply.github.com>
2023-09-19 13:53:13 +0300

    update HW4

---
+++ HW4/HW4/SayfaY.swift
@@ -0,0 +1,28 @@
+//
+//  SayfaY.swift
+//  HW4
+//
+//  Created by Ahmet Emin Yalçınkaya on 19.09.2023.
+//
+
+import UIKit
+
+class SayfaY: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+	
+	@IBAction func buttonGeri(_ sender: Any) {
+		self.navigationController?.popToRootViewController(animated: true)
+		}
+	
+	
+	
+
+}
---
+++ HW4/HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Ahmet Emin Yalçınkaya on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
